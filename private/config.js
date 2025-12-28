b2958e79ad47ff4b3be5e461277f6eae27cee8f1
Zaina Juliette <flmtvnetwork@gmail.com>
2025-12-27 18:50:30 -0800

    forAI2Work

---
+++ private/config.js
@@ -0,0 +1,10 @@
+// FLM TV API Configuration
+// DO NOT commit this file to GitHub!
+// Add this file to .gitignore
+
+const API_KEYS = {
+    OPENAI: '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',
+    TMDB: '34773a6e0f80645a5b07a1ee76ecc26b',
+    OMDB: '266aeb44',
+    YOUTUBE: 'AIzaSyBJMZDXDPcVPUFhMKdYrABrke4PQZgRsT8'
+};
