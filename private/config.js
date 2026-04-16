7e1b6373d67152b768111ad28d30854de4a9fd00
Zaina Juliette <flmtvnetwork@gmail.com>
2026-04-15 18:47:06 -0700

    Update config.js

--- private/config.js
+++ private/config.js
@@ -6,7 +6,7 @@ const API_KEYS = {
     OPENAI: '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',
     TMDB: '34773a6e0f80645a5b07a1ee76ecc26b',
     OMDB: '266aeb44',
-    YOUTUBE: 'AIzaSyBJMZDXDPcVPUFhMKdYrABrke4PQZgRsT8'
+    YOUTUBE: 'AIzaSyDmMHjQL14WW4Pd2TB9mdJafX8JILXeK9Y'
 };
 
 // ================================================
