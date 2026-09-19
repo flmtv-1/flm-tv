687915e787df65746426105150d0662c7a6cf95c
Zaina Juliette <flmtvnetwork@gmail.com>
2026-09-19 07:28:36 -0700

    Update config.js

--- private/config.js
+++ private/config.js
@@ -3,7 +3,6 @@
 // Add this file to .gitignore
 
 const API_KEYS = {
-    OPENAI: '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',
     TMDB: '34773a6e0f80645a5b07a1ee76ecc26b',
     OMDB: '266aeb44',
     YOUTUBE: 'AIzaSyBWX6uRw9I346FzMy75m91O6yKnvuFJq6g'
