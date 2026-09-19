a00f713711940d89b10981f022fe10bf93e83b51
Zaina Juliette <flmtvnetwork@gmail.com>
2026-09-19 07:16:14 -0700

    upday api key

--- private/config.js
+++ private/config.js
@@ -6,7 +6,7 @@ const API_KEYS = {
     OPENAI: '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',
     TMDB: '34773a6e0f80645a5b07a1ee76ecc26b',
     OMDB: '266aeb44',
-    YOUTUBE: 'AIzaSyDmMHjQL14WW4Pd2TB9mdJafX8JILXeK9Y'
+    YOUTUBE: 'AIzaSyBWX6uRw9I346FzMy75m91O6yKnvuFJq6g'
 };
 
 // ================================================
